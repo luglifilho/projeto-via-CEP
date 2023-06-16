c00a50cbfc42117a8ccd3a386576dbdc7d671205
luglifilho <luglifilho@hotmail.com>
2023-06-15 22:29:41 -0300

    .

--- js/controllers/formController.js
+++ js/controllers/formController.js
@@ -1,8 +1,38 @@
+import Address from "../models/address";
+
+function State(){
+
+    this.address = new Address;
+    
+    this.btnSave = null;
+    this.btnClear = null;
+
+    this.inputCep = null;
+    this.inputStreet = null;
+    this.inputNumber = null;
+    this.inputCity = null;
+
+    this.errorCep = null;
+    this.errorNumber = null;
+}
+
+const state = new State();
 
 
 export function init() {
 
-    const inputCep = document.forms.newAdress.cep;
+   state.inputCep = document.forms.newAdress.cep;
+   state.inputStreet = document.forms.newAdress.street;
+   state.inputNumber = document.forms.newAdress.number;
+   state.inputCity = document.forms.newAdress.city;
 
-    console.log(inputCep);
+   state.btnSave = document.forms.newAdress.btnSave;
+   state.btnSave = document.forms.newAdress.btnClear;
+
+   state.errorCep = document.querySelector('[data-error="cep"]');
+   state.errorNumber = document.querySelector('[data-error="number"]');
+
+
+
+    console.log(state);
 }
---
+++ js/models/address.js
@@ -0,0 +1,9 @@
+
+
+export default function Address(cep, stree, number, city){
+    this.cep =cep;
+    this.street = street;
+    this.number = number;
+    this.city = city;
+
+}
